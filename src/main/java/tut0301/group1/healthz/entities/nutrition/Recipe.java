3946b613251ae92d6cb637e5148d186234efcaf3
Meemom <jemima.silaen@gmail.com>
2025-11-02 17:10:41 -0500

    recipe class

--- src/main/java/tut0301/group1/healthz/entities/nutrition/Recipe.java
+++ src/main/java/tut0301/group1/healthz/entities/nutrition/Recipe.java
@@ -3,5 +3,23 @@ package tut0301.group1.healthz.entities.nutrition;
 /**
  * Entity: Minimal recipe.
  */
-public record Recipe(String id, String name, Macro macro) {
+public record Recipe (String recipeId, String name, String description, List<String> instructions,
+                     int prepTime, int cookTime, int servings, String cuisine, String difficulty, String imageurl,
+                     float healthscore, list<String> tags) {
+
+    public int getCalories () {
+        return -1;
+    }
+
+    public Macro getMacros () {
+        return null;
+    }
+
+    public int getCaloriesPerServing () {
+        return -1;
+    }
+
+    public Macro getMacrosPerServing () {
+        return null;
+    }
 }
