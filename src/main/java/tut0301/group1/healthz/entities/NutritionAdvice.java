52c966bd8402d22bf30a3e8813ec3ed22f7b62de
peivand-moradi <peivand.moradi@gmail.com>
2025-11-07 21:00:08 -0500

    updated with nutrition and metrics calculators

--- src/main/java/tut0301/group1/healthz/entities/NutritionAdvice.java
+++ src/main/java/tut0301/group1/healthz/entities/NutritionAdvice.java
@@ -4,20 +4,16 @@ public class NutritionAdvice {
     private double calories;
     private double proteinGrams;
     private double fibreGrams;
-    private double carbohydrateGrams;
     private double fatGrams;
 
-    public NutritionAdvice(double calories, double protein, double fibre, double carbohydrate,
-                           double fat) {
+    public NutritionAdvice(double calories, double protein, double fibre, double fat) {
         this.calories = calories;
         this.proteinGrams = protein;
         this.fibreGrams = fibre;
-        this.carbohydrateGrams = carbohydrate;
         this.fatGrams = fat;
     }
     public double getCalories() { return calories; }
     public double getProtein() { return proteinGrams; }
     public double getFiber() { return fibreGrams; }
-    public double getCarbohydrate() { return carbohydrateGrams; }
     public double getFat(){ return fatGrams;}
 }
