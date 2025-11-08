d093eac587e76a6255e43303b9cdc773c4a5109a
MahanNoosh <mahan.mnnr@gmail.com>
2025-11-08 03:00:46 -0500

    all branches merged

--- src/main/java/tut0301/group1/healthz/entities/recipe/FavoriteRecipe.java
+++ src/main/java/tut0301/group1/healthz/entities/recipe/FavoriteRecipe.java
@@ -1,28 +1,28 @@
-package.tut0301.group1.healthz.entities.recipe;
-
-import tut0301.group1.healthz.entities.nutrition.Recipe;
-import tut0301.group1.healthz.entities.user.UserId;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
-/**
- * Represents Recipe method to favourite different recipes by the user.
- */
-
-public class FavoriteRecipe {
-    private String favoriteId;
-    private UserId userId;
-    private Recipe recipe;
-    private String collectionName;
-    private List<String> tags;
-    private DateTime savedAt;
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-}
+//package tut0301.group1.healthz.entities.recipe;
+//
+//import tut0301.group1.healthz.entities.nutrition.Recipe;
+//import tut0301.group1.healthz.entities.user.UserId;
+//
+//import java.time.LocalDateTime;
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+//import java.util.Objects;
+//import java.util.stream.Collectors;
+//
+///**
+// * Represents Recipe method to favourite different recipes by the user.
+// */
+//
+//public class FavoriteRecipe {
+//    private String favoriteId;
+//    private UserId userId;
+//    private Recipe recipe;
+//    private String collectionName;
+//    private List<String> tags;
+//    private DateTime savedAt;
+//
+//    public Recipe getRecipe() {
+//        return recipe;
+//    }
+//}
