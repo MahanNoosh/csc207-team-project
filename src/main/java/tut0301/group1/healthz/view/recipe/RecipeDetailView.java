fc6f989255d030e761300fedbc1058091e48b4d6
Meemom <jemima.silaen@gmail.com>
2025-11-22 23:03:18 -0500

    recipe detail view

---
+++ src/main/java/tut0301/group1/healthz/view/recipe/RecipeDetailView.java
@@ -0,0 +1,27 @@
+package tut0301.group1.healthz.view.recipe;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import tut0301.group1.healthz.entities.nutrition.Recipe;
+
+public class RecipeDetailView {
+    Scene scene;
+    String recipeName;
+    String imageUrl;
+
+
+    public RecipeDetailView() {
+
+    }
+
+
+}
