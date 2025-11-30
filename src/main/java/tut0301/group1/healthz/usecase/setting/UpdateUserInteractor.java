30d1c6716cf4b395fe868b01c4e595ccdc902014
MahanNoosh <mahan.mnnr@gmail.com>
2025-11-29 19:12:09 -0500

    made update user usecase

---
+++ src/main/java/tut0301/group1/healthz/usecase/setting/UpdateUserInteractor.java
@@ -0,0 +1,30 @@
+package tut0301.group1.healthz.usecase.setting;
+
+import tut0301.group1.healthz.entities.Profile;
+import tut0301.group1.healthz.usecase.dashboard.UserDataDataAccessInterface;
+
+public class UpdateUserInteractor implements UpdateUserInputBoundary {
+
+    private final UserDataDataAccessInterface userDataDAO;
+    private final UpdateUserOutputBoundary presenter;
+
+    public UpdateUserInteractor(UserDataDataAccessInterface userDataDAO,
+                                UpdateUserOutputBoundary presenter) {
+        this.userDataDAO = userDataDAO;
+        this.presenter = presenter;
+    }
+
+    @Override
+    public void updateUser(UpdateUserInputData inputData) throws Exception {
+        Profile profile = inputData.getNewProfile();
+
+        // Update or create user data in Supabase
+        Profile updated = userDataDAO.updateCurrentUserProfile(profile);
+
+        // Prepare output
+        UpdateUserOutputData outputData = new UpdateUserOutputData(updated);
+
+        // Present response
+        presenter.prepareSuccessView(outputData);
+    }
+}
